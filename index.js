ecedce64ca698e7de24dc495b5fd529e2bbc1597
Jonathan Valdesuso <jbvaldesuso@gmail.com>
2017-12-22 00:31:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+}
